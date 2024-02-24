6376e6075b96fa5952e9ed90a03064c79e75bc37
ganggas <subhannizar25@gmail.com>
2024-02-24 18:33:05 +0800

    feat: Add healty route

--- src/factory.py
+++ src/factory.py
@@ -7,6 +7,22 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(1, current_dir)
 
 
+def ping():
+    return "pong"
+
+
+def init_healty_route(app: Flask) -> None:
+    """
+    Initializes a health check route for the given Flask app.
+    Parameters:
+        app (Flask): The Flask app to which the health check
+            route will be added.
+    Returns:
+        None
+    """
+    app.add_url_rule("/ping", view_func=ping)
+
+
 def create_app() -> Flask:
     """
     Create and return a new Flask application.
@@ -15,6 +31,7 @@ def create_app() -> Flask:
         Flask: A new instance of the Flask application.
     """
     app = Flask(__name__)
+    init_healty_route(app)
     return app
 
 
--- src/tests/test_factory.py
+++ src/tests/test_factory.py
@@ -17,3 +17,8 @@ class TestCreateFlask(unittest.TestCase):
 
     def test_debug_mode(self):
         self.assertFalse(self.app.debug, "Ensure the app is not in debug mode")
+
+    def test_healty(self):
+        response = self.app.test_client().get("/ping")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b"pong")
