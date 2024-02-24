dc1b1d86c5d0153f1efb7b1decae63d463b1e00c
ganggas <subhannizar25@gmail.com>
2024-02-25 06:46:40 +0800

    feat: Add base entity model

--- src/core/entity.py
+++ src/core/entity.py
@@ -1,7 +1,24 @@
+from datetime import datetime
 from typing import Type
 
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import DateTime, Integer, func
+from sqlalchemy.orm import Mapped, declarative_base, mapped_column
 
 Base = declarative_base()
 
 BaseType = Type[Base]
+
+
+class BaseEntityWithIdInteger(Base):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+
+class BaseTimestampEntity(Base):
+    __abstract__ = True
+
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, server_default=func.now(), server_onupdate=func.now()
+    )
