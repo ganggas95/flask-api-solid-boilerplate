06dc004b59a7c8583ff48cea7a1d645e4c787ac5
ganggas <subhannizar25@gmail.com>
2024-02-24 18:26:56 +0800

    chore: Initialize conftest for reusable app across tests

---
+++ src/tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from flask import Flask
+
+from src.factory import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update({"TESTING": True, "DEBUG": False})
+    yield app
+
+
+@pytest.fixture
+def client(app: Flask):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app: Flask):
+    return app.test_cli_runner()
